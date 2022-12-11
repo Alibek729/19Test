df2ed9b37635dde32b9eb22ed7360406ee2cf153
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2022-12-11 20:35:02 +0600

    Initial Commit

---
+++ MyPostRequest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPostRequest
+//
+//  Created by Alibek Kozhambekov on 11.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
